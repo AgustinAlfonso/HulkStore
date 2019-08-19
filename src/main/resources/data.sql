452a7b64abff48300e5baf946f0375f6de1b336f
AgustinAlfonso <alfonsoagustin07@gmail.com>
2019-08-18 23:53:17 -0300

    readme, sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into PRODUCT (id, amount, product_name, product_type) values
-(99, 10, 'Comic Iron Man', 'Comic'),
-(33, 10, 'Figura Spiderman', 'Figura de Accion'),
-(44, 10, 'Disfraz de Thor', 'Disfraz'),
-(55, 10, 'Comic Capitan Barato', 'Comic'),
-(65, 10, 'Taza Batman', 'Taza'),
-(76, 10, 'Trading Card Pokemon', 'Trading Card'),
-(87, 10, 'Comic Liga de la Justicia', 'Comic'),
-(98, 10, 'Disfraz Chapulin Colorado', 'Disfraz');
+(999, 10, 'Comic Iron Man', 'Comic'),
+(998, 10, 'Figura Spiderman', 'Figura de Accion'),
+(997, 10, 'Disfraz de Thor', 'Disfraz'),
+(996, 10, 'Comic Capitan Barato', 'Comic'),
+(995, 10, 'Taza Batman', 'Taza'),
+(994, 10, 'Trading Card Pokemon', 'Trading Card'),
+(993, 10, 'Comic Liga de la Justicia', 'Comic'),
+(992, 10, 'Disfraz Chapulin Colorado', 'Disfraz');
